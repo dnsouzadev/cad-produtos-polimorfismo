ad3b2ffffc3943b2cf86cff78097e66310523f03
Daniel Souza <workdndsza@gmail.com>
2023-09-20 22:30:55 -0300

    criando a classe de produto importado

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
+import entities.ImportedProduct;
+import entities.Product;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Product produto = new Product("Iphone", 2500.00);
+        ImportedProduct produto1 = new ImportedProduct("Iphone", 2500.00, 200.00);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println(produto.priceTag());
+        System.out.println(produto1.priceTag());
     }
 }
---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class ImportedProduct extends Product {
+    public Double customsFee;
+
+    public ImportedProduct(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    public ImportedProduct(String name, Double price, Double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public Double totalPrice(Double customsFee) {
+        return price + customsFee;
+    }
+
+    @Override
+    public String priceTag() {
+        return getName() + " $ " + totalPrice(customsFee);
+    }
+}
